3f3de0f588022812ea135efad33e883815e4e8ef
atishay5845 <atishaysodhiya5845@gmail.com>
2024-09-13 15:51:23 +0530

    converting a 1D array to Doubly Linked List

---
+++ P22.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+class node{
+    public:
+    int data;
+    node* next;
+    node* back;
+    public:
+    node(int data1,node* next1,node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    public:
+    node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+};
+node* convertArr2DLL(vector<int> &arr){
+    node* head= new node(arr[0]);
+    node* prev = head;
+    for(int i=1;i<arr.size();i++){
+        node* temp = new node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+void printLL(node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    node* head = convertArr2DLL(arr);
+    printLL(head);
+    
+    return 0;
+}
+
