1897073ed7d9d81deba78b809c938768100fa0fb
atishay5845 <atishaysodhiya5845@gmail.com>
2024-09-15 23:13:49 +0530

    Converting array to LL

---
+++ P11.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* convertArr2LL(vector<int>& arr) {
+    Node* head = new Node(arr[0]);
+    Node* move = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        move->next=temp;
+        move=temp;
+    }
+    return head;
+}
+void printLL(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    Node* head= convertArr2LL(a);
+    cout<<"Created Linked list: ";
+    printLL(head);
+    
+    
+}
