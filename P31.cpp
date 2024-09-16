35472acb26a2d2bbc6eb133a53754e1458a23ab3
atishay5845 <atishaysodhiya5845@gmail.com>
2024-09-16 22:25:53 +0530

    Reversing LL by optimal approach 1

---
+++ P31.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* convertArr2LL(vector<int>& arr) {
+    Node* head = new Node(arr[0]);
+    Node* move = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        move->next=temp;
+        move=temp;
+    }
+    return head;
+}
+Node* reverseLL(Node* head){
+    Node* temp=head;
+    Node* prev = NULL;
+    while(temp){
+        Node* front = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = front;
+    }
+    return prev;
+}
+void printLL(Node* head){
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    Node* head= convertArr2LL(a);
+    cout<<"Created Linked list: ";
+    printLL(head);
+    
+    head = reverseLL(head);
+    cout<<endl<<"Reversed Linked List: ";
+    printLL(head);
+}
